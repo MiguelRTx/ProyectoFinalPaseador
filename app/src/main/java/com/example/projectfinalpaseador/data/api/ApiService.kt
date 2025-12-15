e6024dbb532eede0e87b7f2cb8d8290ed2760b09
MiguelRTx <mariacamiguel962@gmail.com>
2025-12-15 11:16:54 -0400

    Paseador

--- app/src/main/java/com/example/projectfinalpaseador/data/api/ApiService.kt
+++ app/src/main/java/com/example/projectfinalpaseador/data/api/ApiService.kt
@@ -4,7 +4,7 @@ import com.example.projectfinalpaseador.data.model.*
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.httpp.*
 
 interface ApiService {
     @POST("auth/walkerlogin")
